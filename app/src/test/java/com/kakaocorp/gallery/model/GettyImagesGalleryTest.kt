a73dae34b36f25b78d2317327f545362d1cb8f13
Jaeho Choe <jaeho.choe@myrealtrip.com>
2020-06-29 21:59:16 +0900

    Test 코드 작성

---
+++ app/src/test/java/com/kakaocorp/gallery/model/GettyImagesGalleryTest.kt
@@ -0,0 +1,19 @@
+package com.kakaocorp.gallery.model
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+/**
+ * Created by jaehochoe on 2020/06/29.
+ */
+const val URL = "https://www.gettyimagesgallery.com/collection/sasha"
+
+class GettyImagesGalleryTest {
+
+    @Test
+    fun `convert model to uri`() {
+        val src = GettyImagesGallery("sasha")
+        assertEquals(src.toUri(), URL)
+    }
+
+}
